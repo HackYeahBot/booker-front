8c39e759e1d0943defc34ca482ec20075a4a5e07
Keikeu <karolina.m.placek@gmail.com>
2019-09-15 10:08:53 +0200

    implement seat selection

--- src/components/SeatChooser.js
+++ src/components/SeatChooser.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import qs from 'query-string';
 import { API } from '../App';
 
+import './SeatChooser.css';
+
+import legend from '../media/legend.jpg';
+import plane from '../media/plane.jpg';
+
 class SeatChooser extends Component {
   async componentDidMount() {
     const data = await this.fetchSeatsByScenario();
@@ -25,7 +30,13 @@ class SeatChooser extends Component {
   }
 
   render() {
-    return <h2>SeatChooser</h2>;
+    return (
+      <div className='seat-chooser'>
+        <h2>SELECT YOUR FAVOURITE SEAT</h2>
+        <img src={plane} alt="plane"/>
+        <img src={legend} alt="legend"/>
+      </div>
+    );
   }
 }
 
